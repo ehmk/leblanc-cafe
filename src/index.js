18b1a17edebfb7b263998792dccc2b5a9bb5df2f
ehmk <ehmk@users.noreply.github.com>
2023-07-29 20:46:23 -0400

    Imported and changed the font

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import navBar from './nav.js';
 import home from './home.js';
 import './styles/global.css';
 
-console.log('Hello world');
-
 const body = document.body;
 let content = document.createElement('div');
 
