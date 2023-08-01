f3bb687b5bef7c34f1a8559427cbc2469df2851a
ehmk <ehmk@users.noreply.github.com>
2023-07-31 23:44:46 -0400

    Added contact information

--- src/contact.js
+++ src/contact.js
@@ -1,8 +1,21 @@
 import './styles/contact.css';
 
+function createContactInfo(textContent, href) {
+  const div = document.createElement('div');
+  const link = document.createElement('a');
+  div.classList.add('contact-info');
+  link.textContent = textContent;
+  link.href = href;
+  div.appendChild(link);
+  return div;
+}
+
 export default function contact() {
-  const contact = document.createElement('div');
-  contact.id = 'contact-module';
-  contact.textContent = 'Contact!';
-  return contact;
+  const div = document.createElement('div');
+  div.id = 'contact-module';
+  div.appendChild(createContactInfo('> Phone: 111-222-4444', '#'));
+  div.appendChild(createContactInfo('> Twitter: @Leblanc_Tokyo', '#'));
+  div.appendChild(createContactInfo('> Instagram: @Leblanc_Tokyo', '#'));
+  div.appendChild(createContactInfo('> Facebook: @Leblanc_Tokyo', '#'));
+  return div;
 }
